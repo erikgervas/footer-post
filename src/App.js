7035a6b0f0495bf943b9ff18189bae7ae9d9ce0f
erikgervas <erikgervas@gmail.com>
2019-02-24 21:41:33 -0300

    Cosas del final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import { Header } from "./Header";
 import { Content } from "./Content";
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export const Content = () => (
-  <div className={`content`}>
+  <div className={ `content` }>
     My awesome content!
   </div>
 );
